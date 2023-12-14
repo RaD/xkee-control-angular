86a24e85a682ac76dea987ecd2247ed1dd9f68f2
Ruslan Popov <ruslan.popov@gmail.com>
2023-12-14 13:35:09 +0300

    Secure local storage

--- src/app/local.service.ts
+++ src/app/local.service.ts
@@ -1,24 +1,48 @@
 import { Injectable } from '@angular/core';
+// import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class LocalService {
+  secret_key: string = '';
 
   constructor() { }
 
-  public saveData(key: string, value: string) {
+  // private encrypt(value: string): string {
+  //   let data = CryptoJS.AES.encrypt(value, this.secret_key);
+  //   return data.toString()
+  // }
+
+  // private decrypt(value: string): string {
+  //   let data = CryptoJS.AES.decrypt(value, this.secret_key);
+  //   return data.toString(CryptoJS.enc.Utf8);
+  // }
+
+  public saveData(key: string, value: string): void {
     localStorage.setItem(key, value);
   }
 
-  public getData(key: string) {
+  // public saveDataSecure(key: string, value: string): void {
+  //   localStorage.setItem(key, this.encrypt(value));
+  // }
+
+  public getData(key: string): string | null {
     return localStorage.getItem(key)
   }
-  public removeData(key: string) {
+
+  // public getDataSecure(key: string): string | null {
+  //   let data: string | null = localStorage.getItem(key);
+  //   if (data == null) return null;
+  //   return this.decrypt(data)
+  // }
+
+  public removeData(key: string): void {
     localStorage.removeItem(key);
   }
 
-  public clearData() {
+  public clearData(): void {
     localStorage.clear();
   }
 }
