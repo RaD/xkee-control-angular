310c256ac93ae7c33eadd6535a4091eb70d58e30
Ruslan Popov <ruslan.popov@gmail.com>
2025-09-16 12:31:29 -0700

    Implement lazy loading for all pages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,41 +1,77 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ConfirmationPage } from '../pages/confirmation/component';
-import { AreaListPage } from '../pages/area_list/component';
-import { AreaPage } from '../pages/area/component';
-import { DeviceListPage } from '../pages/device_list/component';
-import { DevicePage } from '../pages/device/component';
-import { CustomerListPage } from '../pages/customer_list/component';
-import { CustomerPage } from '../pages/customer/component';
-import { ImporterComponent } from '../pages/importer/component';
-import { LinkingPage } from '../pages/linking/component';
-import { PaymentPage } from '../pages/payment/component';
-import { BleListPage } from '../pages/ble_list/component';
-import { BleDevicePage } from '../pages/ble_device/component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'confirm/areas/:area_pk/:entity/:entity_pk', component: ConfirmationPage},
-    {path: 'confirm/areas/:area_pk', component: ConfirmationPage},
-    {path: 'areas/:area_pk/customer/:customer_pk/payment', component: PaymentPage},
-    {path: 'areas/:pk/ble/device/:device_id', component: BleDevicePage},
-    {path: 'areas/:pk/ble', component: BleListPage},
-    {path: 'areas/:pk/customer/:customer_pk/linking', component: LinkingPage},
-    {path: 'areas/:pk/customer/:customer_pk/:action', component: CustomerPage},
-    {path: 'areas/:pk/customers/create', component: CustomerPage},
-    {path: 'areas/:pk/customers', component: CustomerListPage},
-    {path: 'areas/:pk/device/:device_pk/:action', component: DevicePage},
-    {path: 'areas/:pk/devices/create', component: DevicePage},
-    {path: 'areas/:pk/devices', component: DeviceListPage},
-    {path: 'areas/:pk/:action', component: AreaPage},
-    {path: 'areas/create', component: AreaPage},
-    {path: 'areas/import', component: ImporterComponent},
-    {path: 'areas', component: AreaListPage},
-    {path: '', pathMatch: 'full', redirectTo: '/areas'},
-    {path: '**', redirectTo: '/areas'},
+    {
+        path: 'confirm/areas/:area_pk/:entity/:entity_pk', 
+        loadComponent: () => import('../pages/confirmation/component').then(m => m.ConfirmationPage)
+    },
+    {
+        path: 'confirm/areas/:area_pk', 
+        loadComponent: () => import('../pages/confirmation/component').then(m => m.ConfirmationPage)
+    },
+    {
+        path: 'areas/:area_pk/customer/:customer_pk/payment', 
+        loadComponent: () => import('../pages/payment/component').then(m => m.PaymentPage)
+    },
+    {
+        path: 'areas/:pk/ble/device/:device_id', 
+        loadComponent: () => import('../pages/ble_device/component').then(m => m.BleDevicePage)
+    },
+    {
+        path: 'areas/:pk/ble', 
+        loadComponent: () => import('../pages/ble_list/component').then(m => m.BleListPage)
+    },
+    {
+        path: 'areas/:pk/customer/:customer_pk/linking', 
+        loadComponent: () => import('../pages/linking/component').then(m => m.LinkingPage)
+    },
+    {
+        path: 'areas/:pk/customer/:customer_pk/:action', 
+        loadComponent: () => import('../pages/customer/component').then(m => m.CustomerPage)
+    },
+    {
+        path: 'areas/:pk/customers/create', 
+        loadComponent: () => import('../pages/customer/component').then(m => m.CustomerPage)
+    },
+    {
+        path: 'areas/:pk/customers', 
+        loadComponent: () => import('../pages/customer_list/component').then(m => m.CustomerListPage)
+    },
+    {
+        path: 'areas/:pk/device/:device_pk/:action', 
+        loadComponent: () => import('../pages/device/component').then(m => m.DevicePage)
+    },
+    {
+        path: 'areas/:pk/devices/create', 
+        loadComponent: () => import('../pages/device/component').then(m => m.DevicePage)
+    },
+    {
+        path: 'areas/:pk/devices', 
+        loadComponent: () => import('../pages/device_list/component').then(m => m.DeviceListPage)
+    },
+    {
+        path: 'areas/:pk/:action', 
+        loadComponent: () => import('../pages/area/component').then(m => m.AreaPage)
+    },
+    {
+        path: 'areas/create', 
+        loadComponent: () => import('../pages/area/component').then(m => m.AreaPage)
+    },
+    {
+        path: 'areas/import', 
+        loadComponent: () => import('../pages/importer/component').then(m => m.ImporterComponent)
+    },
+    {
+        path: 'areas', 
+        loadComponent: () => import('../pages/area_list/component').then(m => m.AreaListPage)
+    },
+    {
+        path: '', 
+        pathMatch: 'full', 
+        redirectTo: '/areas'
+    },
+    {
+        path: '**', 
+        redirectTo: '/areas'
+    },
 ];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes, {enableTracing: true})],
-    exports: [RouterModule],
-})
-export class AppRoutingModule {}
