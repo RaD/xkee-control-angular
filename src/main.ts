7be51239a8c147e9e3a1c449bb4dd7feb96a2d7c
Ruslan Popov <ruslan.popov@gmail.com>
2025-09-16 09:49:27 -0700

    Fix Angular localize import warning

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import '@angular/localize/init';  // для локализации
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/config';
 import { AppComponent } from './app/component';
